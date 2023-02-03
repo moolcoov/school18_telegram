0e3adafecfa298c3fc9851866ebdfe78d43ace1a
Stepanov Ilya <rukzzak@mail.ru>
2023-02-03 16:41:32 +0800

    Add single images and reposts

--- data/vk_group_parser.py
+++ data/vk_group_parser.py
@@ -1,4 +1,5 @@
 import vk
+
 from data import constants as const
 
 
@@ -8,13 +9,23 @@ class VkGroupParser:
         self.last_post_id = open("./data/last_post.txt").read()
 
     def get_last_post(self):
-        post = self.vk_api.wall.get(owner_id=const.GROUP_ID, count=3, v=const.V)['items'][0]
-        post_id = post['id']
+        try:
+            post = self.vk_api.wall.get(owner_id=const.GROUP_ID, count=1, v=const.V)['items'][0]
+            post_id = post['id']
+        except Exception as e:
+            print(e)
+            self.vk_api = vk.API(const.API_KEY)
+            self.get_last_post()
 
         if int(post_id) != int(self.last_post_id):
             self.last_post_id = post_id
             with open("./data/last_post.txt", "w") as f:
                 f.write(str(self.last_post_id))
+
+            if 'copy_history' in post:
+                base_text = post['text'] + "\n\n"
+                post = post['copy_history'][0]
+                post['text'] = base_text + post['text']
 
             return post
 
